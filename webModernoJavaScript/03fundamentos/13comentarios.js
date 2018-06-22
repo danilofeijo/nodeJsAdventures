ef0b2747db7dbe94e075b21f3d65afc251c43848
Danilo Silva Feijo de Sousa <danilofs@lnxcit015748.cit>
2018-06-22 00:00:33 -0300

    Section 3 - Primeros exercicios

---
+++ webModernoJavaScript/03fundamentos/13comentarios.js
@@ -0,0 +1,15 @@
+// COmnetario de uma linha
+console.log('Linha 1');
+
+/*
+Comentario de 
+multiplas linhas
+*/
+console.log('Linha 2');
+
+/*
+ * Estilo diferente
+ * de comentario
+ * de multiplas linhas
+ */
+console.log('Linha 3');
