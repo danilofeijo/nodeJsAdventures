f9e94e757239484e4744c6f160183b04c6dca8ac
Danilo Silva Feijo de Sousa <danilofs@lnxcit015748.cit>
2018-06-26 20:25:45 -0300

    improvement - change language for folder and file names - 2 of 2

---
+++ webModernoJavaScript/03fundamentals/15weakTyping.js
@@ -0,0 +1,12 @@
+let cachorro = 'Legal';
+console.log(cachorro);
+console.log(typeof cachorro);
+
+cachorro = 2.345
+console.log(cachorro);
+console.log(typeof cachorro);
+
+// Evite nome generico e siglas para variaveis
+let valor = 'a';
+let numero = 1;
+let pqp = false // Produto Quimico Perigoso... kk
