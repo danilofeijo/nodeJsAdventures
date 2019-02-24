eda3086f57dc7d6c577ba52dc23c11799131ab74
Danilo Silva Feijo de Sousa <danilofs@lnxcit015748.cit>
2019-02-24 18:52:59 -0300

    Section 3 - Destructing operators part 2

---
+++ webModernoJavaScript/03fundamentals/43operatorsDestructing04.js
@@ -0,0 +1,11 @@
+function rand([min = 0, max = 1000]) {
+  if (min > max) [min, max] = [max, min]
+  const valor = Math.random() * (max - min) + min;
+  return Math.floor(valor);
+}
+
+console.log(rand([50, 100]));
+console.log(rand([100, 50]));
+console.log(rand([50]));
+console.log(rand([, 5]));
+console.log(rand([]));
