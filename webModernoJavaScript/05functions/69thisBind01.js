bda9cd0fa779d6c256c27791340dc2b914c4e1ef
Danilo Silva Feijo de Sousa <danilofs@lnxcit014468.cit>
2019-07-26 17:18:12 -0300

    05functions - Comment enhancements

--- webModernoJavaScript/05functions/69thisBind01.js
+++ webModernoJavaScript/05functions/69thisBind01.js
@@ -5,9 +5,13 @@ const person = {
   }
 }
 
+// It works!
 person.talk()
+
 const talk = person.talk
 talk() // It doesn't work
+
+// It works!
 
 const talkAboutPeople = person.talk.bind(person)
 talkAboutPeople()
